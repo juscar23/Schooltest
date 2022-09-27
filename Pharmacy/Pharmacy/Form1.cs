ac8c95b8504786b4cea260c27746fbe3fe8b9f32
juscar23 <113036054+juscar23@users.noreply.github.com>
2022-09-27 20:15:27 +0800

    Add files via upload

---
+++ Pharmacy/Pharmacy/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy
+{
+    public partial class frmpharmacy : Form
+    {
+        public frmpharmacy()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.Transparent;
+           pictureBox2.BackColor = Color.Transparent;
+        }
+
+        private void frmpharmacy_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
